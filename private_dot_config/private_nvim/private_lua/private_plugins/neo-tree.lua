d4c7d12870e6b7098a4e9b2213b995ec1b8e2d09
Matthias Baumgart <matthias@briefhansa.de>
2023-12-24 00:08:03 +0100

    Nvim neo-tree set keymap to toggle

--- private_dot_config/private_nvim/private_lua/private_plugins/neo-tree.lua
+++ private_dot_config/private_nvim/private_lua/private_plugins/neo-tree.lua
@@ -7,7 +7,7 @@ return {
     "MunifTanjim/nui.nvim",
   },
   config = function()
-    vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
+    vim.keymap.set('n', '<C-n>', ':Neotree toggle<CR>', {})
   end
 }
 
