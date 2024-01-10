9f196769b33c9520d7336f6a0f7cbfa02d420e98
Matthias Baumgart <matthias@briefhansa.de>
2024-01-10 16:52:13 +0100

    Nvim set mason to auto install lsp plugin

--- private_dot_config/private_nvim/private_lua/private_plugins/lsp-config.lua
+++ private_dot_config/private_nvim/private_lua/private_plugins/lsp-config.lua
@@ -7,11 +7,10 @@ return {
   },
   {
     "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "tsserver" }
-      })
-    end
+    lazy = false,
+    opts = {
+      auto_install = true,
+    }
   },
   {
     "neovim/nvim-lspconfig",
