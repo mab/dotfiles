b5c9774803def31e4cc8c9b722ff8d56cf6e9adb
Matthias Baumgart <matthias@briefhansa.de>
2024-02-27 14:04:21 +0100

    Extract zsh completions

---
+++ private_dot_config/private_zsh/private_completions.zsh
@@ -0,0 +1,5 @@
+[[ $commands[chezmoi] ]] && source <(chezmoi completion zsh 2> /dev/null)
+[[ $commands[fzf] ]] && source "$(brew --prefix)/opt/fzf/shell/completion.zsh" 2> /dev/null
+[[ $commands[kubectl] ]] && source <(kubectl completion zsh 2> /dev/null)
+[[ $commands[konf] ]] && source <(konf completion zsh 2> /dev/null)
+[[ $commands[mise] ]] && source <(mise completion zsh 2> /dev/null)
