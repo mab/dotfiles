85c37ed1dfbddc009187dea660d5471386509824
Matthias Baumgart <matthias@briefhansa.de>
2024-08-21 13:53:54 +0200

    Add docker zsh completion

--- private_dot_config/private_zsh/private_completions.zsh
+++ private_dot_config/private_zsh/private_completions.zsh
@@ -1,5 +1,6 @@
 [[ $commands[aqua] ]] && source <(aqua completion zsh 2> /dev/null)
 [[ $commands[chezmoi] ]] && source <(chezmoi completion zsh 2> /dev/null)
+[[ $commands[docker] ]] && source <(docker completion zsh 2> /dev/null)
 [[ $commands[fzf] ]] && source "$(brew --prefix)/opt/fzf/shell/completion.zsh" 2> /dev/null
 [[ $commands[kubectl] ]] && source <(kubectl completion zsh 2> /dev/null)
 [[ $commands[konf] ]] && source <(konf completion zsh 2> /dev/null)
