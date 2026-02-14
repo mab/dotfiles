a6ea479088483cab439568c9a9e4b6360c0d7832
Matthias Baumgart <matthias@briefhansa.de>
2026-02-14 13:57:01 +0100

    Neovim config: disalbe LuaRocks

--- private_dot_config/private_nvim/init.lua
+++ private_dot_config/private_nvim/init.lua
@@ -14,5 +14,9 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins")
+require("lazy").setup("plugins", {
+  rocks = {
+    enabled = false,
+  },
+})
 
