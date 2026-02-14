bafe8754894de9b004d0feeff0ebd9ec6eaaa7bc
Matthias Baumgart <matthias@briefhansa.de>
2026-02-14 06:27:15 +0100

    Update neovim lsp config

--- private_dot_config/private_nvim/private_lua/private_plugins/lsp-config.lua
+++ private_dot_config/private_nvim/private_lua/private_plugins/lsp-config.lua
@@ -1,15 +1,24 @@
 return {
   {
     "williamboman/mason.nvim",
+    dependencies = {
+      "WhoIsSethDaniel/mason-tool-installer.nvim",
+    },
     config = function()
       require("mason").setup()
-    end
+      require("mason-tool-installer").setup({
+        ensure_installed = {
+          "stylua",
+        },
+      })
+    end,
   },
   {
     "williamboman/mason-lspconfig.nvim",
     lazy = false,
     opts = {
       auto_install = true,
+      ensure_installed = { "ts_ls", "html", "lua_ls" },
     }
   },
   {
@@ -17,16 +26,9 @@ return {
     config = function()
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      local lspconfig = require("lspconfig")
-      lspconfig.tsserver.setup({
-        capabilities = capabilities
-      })
-      lspconfig.html.setup({
-        capabilities = capabilities
-      })
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities
-      })
+      vim.lsp.config("ts_ls", { capabilities = capabilities })
+      vim.lsp.config("html", { capabilities = capabilities })
+      vim.lsp.config("lua_ls", { capabilities = capabilities })
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
